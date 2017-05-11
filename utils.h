1f24d1991df9ef2fb2912ec24b0cf25a321a0054
Luca Corinzia <luca.corinzia@gmail.com>
2017-05-11 22:04:31 +0200

    fix error uint4x1_t

--- utils.h
+++ utils.h
@@ -7,6 +7,6 @@ int inline saturate(float a);
 void quantize_parameter(float min, float max, float *scale, float *zero_point);
 void get_min_max(float *d,int rows,int columns, float *min, float *max);
 
-typedef struct unsigned_4_bit unit4x1_t;
+typedef struct unsigned_4_bit uint4x1_t;
 
 #endif
