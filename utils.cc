3f35bf4bdec3f34da385adff145f652fc00a4794
Luca Corinzia <luca.corinzia@gmail.com>
2017-05-11 21:39:16 +0200

    fix error get_min_max

--- utils.cc
+++ utils.cc
@@ -19,7 +19,8 @@ void quantize_parameter(float min, float max, float *scale, float *zero_point){
 }
 
 void get_min_max(float *d,int rows,int columns, float *min, float *max){
-	
+	*max = d[0];
+	*min = *max;
 	for(int i = 0; i<rows; i++){
 		for(int j = 0; j<columns; j++){
 			el = d[i*rows+columns];
