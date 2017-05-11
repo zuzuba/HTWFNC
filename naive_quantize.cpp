74880a3e9f0937e2b7b10876161e050ad7718101
Luca Corinzia <luca.corinzia@gmail.com>
2017-05-11 22:45:59 +0200

    upload vanilla quantize

--- naive_quantize.cpp
+++ naive_quantize.cpp
@@ -4,57 +4,27 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include "utils.h"
 #include "naiv.h"
 
-double max(double *a, double *b){
-	return *a < *b ? *b : *a;
-}
 
-double min(double *a, double *b){
-	return *a < *b ? *a : *b;
-}
+void vanilla_quantize(float *d, uint4x1_t *q, float *min, float *max, int rows, int columns){
+	
+	float scale, zero_point;
+	get_min_max(d,rows,columns,&min,&max);
+	quantize_parameter(min,max,&scale,&zero_point);
 
-void vanilla_quantize(double **d, uint8_t **q, double *mn, double *mx, int n){
-	*mx = INT_MIN, *mn = INT_MAX;
+	float t;
 
-	// Find min and max of matrix
-	for(int i = 0; i<n; i++){
-		for(int j = 0; j<n; j++){
-			*mx = max(mx,&d[i][j]);
-			*mn = min(mn,&d[i][j]);
+	for(int i = 0; i<rows; i++){
+		for(int j = 0; j<columns; j++){
+			t = zero_point + d[i*rows+columns]/scale;
+			q[i*rows+columns].i = (uint8_t)round(saturate(t));
 		}
 	}
-
-	// Compute size of linear cell
-	double delta = (*mx - *mn)/255;
-	double offset = -*mn/delta;
-	
-	uint8_t int_offset;
-	if (offset < 0){
-		int_offset = 0;
-	} else if (offset > 255){
-		int_offset = 255;
-	} else {
-		int_offset = (uint8_t)round(offset);
-	}
-
-	double qmin = 0 ;
-	double qmax = 255;
-	double t1, t2, t3;
-	// Determine uint8_t representation
-	for(int i = 0; i<n; i++){
-		for(int j = 0; j<n; j++){
-			t1 = int_offset + d[i][j]/delta;
-			t2 = min(&qmax, &t1);
-			t3 = max(&qmin, &t2);
-
-			q[i][j] = (uint8_t)round(t3);
-	}
-
-
 }
-}
+
+
 void dequantize(double **d, uint8_t **q, double *mn, double *mx, int n){
 	double delta = (*mx - *mn)/256; // size of linear cell
 	double runner = 0.0;
@@ -62,50 +32,3 @@ void dequantize(double **d, uint8_t **q, double *mn, double *mx, int n){
 		for(int j = 0; j<n; j++)
 			d[i][j] = *mn + q[i][j]*delta; // if q[i][j] == 0, then d[i][j] == mn
 }
-
-// int main() {
-// 	int n=3;
-// 	double **d = (double **)malloc( n * sizeof(double *) );
-// 	double **ded = (double **)malloc( n * sizeof(double *) );
-// 	uint8_t **q = (uint8_t **)malloc( n*n * sizeof(uint8_t) );
-// 	for (int i = 0; i<n; i++) {
-// 		d[i] = (double *)malloc( n * sizeof(double) );
-// 		ded[i] = (double *)malloc( n * sizeof(double) );
-// 		q[i] = (uint8_t *)malloc( n * sizeof(uint8_t) );
-// 	}
-
-// 	for(int i = 0; i<n; i++){
-// 		for(int j = 0; j<n; j++)
-// 			d[i][j] = i*j+2;
-// 	}
-// 	double mn,mx;
-// 	quantize(d,q,&mn,&mx,n);
-// 	dequantize(ded,q,&mn,&mx,n);
-
-// 	for(int i = 0; i<n; i++){
-// 		for(int j = 0; j<n; j++){
-// 			printf("%f ", d[i][j]);
-// 		}
-// 		printf("\n");
-// 	}
-
-// 	for(int i = 0; i<n; i++){
-// 		for(int j = 0; j<n; j++){
-// 			printf("%d ", q[i][j]);
-// 		}
-// 		printf("\n");
-// 	}
-
-// 	for(int i = 0; i<n; i++){
-// 		for(int j = 0; j<n; j++){
-// 			printf("%f ", ded[i][j]);
-// 		}
-// 		printf("\n");
-// 	}
-
-
-
-// 	//insert validation framework here...
-// }
-
-
