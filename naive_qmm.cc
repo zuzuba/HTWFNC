b8d5b837e4332bd1521905f96dc1157e2de7aade
Matteo Turchetta <matteotu@inf.ethz.ch>
2017-05-11 20:40:30 +0200

    Begininning of 4 bits MMM

--- naive_qmm.cc
+++ naive_qmm.cc
@@ -1,13 +1,26 @@
-void qmmm( float lhs_scale, uint8_t lhs_offset, float rhs_scale,uint8_t rhs_offset, uint8_t* lhs_int_mat, uint8_t* rhs_int_mat, float result_scale, uint8_t result_offset, uint8_t* result_int_mat, int n,int k, int m ){
+#include <stdio.h>
+#include <stdint.h>
+
+
+// Struct to handle integer of 4 bits at a time
+typedef struct {
+   uint8_t i1 : 4;
+   uint8_t i2 : 4;
+} unit4x2_t;
+
+void qmmm8bits( float lhs_scale, uint8_t lhs_offset, float rhs_scale,uint8_t rhs_offset, uint8_t* lhs_int_mat, uint8_t* rhs_int_mat, float result_scale, uint8_t result_offset, uint8_t* result_int_mat, int n,int k, int m ){
 
 	int accumulator;
 
 	for(int i=0;i<n;i++){
 		for(int j=0;j<m;j++){
 			for(int t=0; t<k;t++){
-				accumulator = lhs_int_mat[i*n + t]*lhs_int_mat[t*m + j];
+				accumulator = (lhs_int_mat[i*n + t] - lhs_offset) * (rhs_int_mat[t*m + j] - rhs_offset);
 			}
-		result_int_mat[i*n+j] = result_offset + (lhs_scale*rhs_scale/result_scale)*accumulator;
+		result_int_mat[i*n+j] = result_offset + (lhs_scale * rhs_scale/result_scale) * accumulator;
 		}
 	}
 }
+
+
+void qmm(float l_scale, float r_scale, float result_scale, unit4x2_t l_offset,  )
