57651a5ec03ea4758fe18d671fc5d811240bf64d
Luca Corinzia <luca.corinzia@gmail.com>
2017-05-11 21:35:08 +0200

    add utils

--- utils.h
+++ utils.h
@@ -1,5 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 float inline max(float a, float b);
 float inline min(float a, float b);
 int inline saturate(float a);
 void quantize_parameter(float min, float max, float *scale, float *zero_point);
 void get_min_max(float *d,int rows,int columns, float *min, float *max);
+
+#endif
