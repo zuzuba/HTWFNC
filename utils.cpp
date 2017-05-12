2326cc9d599c936719e82f44b93803e06fd21c69
Matteo Turchetta <matteotu@inf.ethz.ch>
2017-05-12 14:18:17 +0200

    Fixed utils and renamed qmm

--- utils.cpp
+++ utils.cpp
@@ -1,11 +1,11 @@
 #include "utils.h"
 
-#define qmin = 0
-#define qmax = 15
+#define qmin 0
+#define qmax 15
 
-struct unsigned_4_bit{
-   uint8_t i : 4;
-};
+// struct unsigned_4_bit{
+//    uint8_t i : 4;
+// };
 
 float inline max(float a, float b){
 	return a < b ? b : a;
@@ -19,19 +19,21 @@ int inline saturate(float a){
 	return (int)max(qmin,min(qmax,a));
 }
 
+
 void quantize_parameter(float min, float max, float *scale, float *zero_point){
 	*scale = (max - min)/(qmax-qmin);
 	*zero_point = -min/(*scale);
 }
 
-void get_min_max(float *d,int rows,int columns, float *min, float *max){
-	*max = d[0];
-	*min = *max;
+void get_min_max(float *d,int rows,int columns, float *mn, float *mx){
+	*mx = d[0];
+	*mn = *mx;
+	float el;
 	for(int i = 0; i<rows; i++){
 		for(int j = 0; j<columns; j++){
 			el = d[i*rows+columns];
-			*max = max(mx,el);
-			*min = min(mn,el);
+			*mx = max(*mx,el);
+			*mn = min(*mn,el);
 		}
 	}
 }
--- utils.h
+++ utils.h
@@ -1,12 +1,16 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdint.h>
+
 float inline max(float a, float b);
 float inline min(float a, float b);
 int inline saturate(float a);
 void quantize_parameter(float min, float max, float *scale, float *zero_point);
 void get_min_max(float *d,int rows,int columns, float *min, float *max);
 
-typedef struct unsigned_4_bit uint4x1_t;
+typedef struct unsigned_4_bit {
+   uint8_t i : 4;
+} uint4x1_t;
 
 #endif
