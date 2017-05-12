01a7381b1d901c166788667adeac1bcca6c87921
Matteo Turchetta <matteotu@inf.ethz.ch>
2017-05-12 15:38:10 +0200

    Header file for naive qmm

---
+++ naive_qmm.h
@@ -0,0 +1,8 @@
+#ifndef NAIVE_QMM_H
+#define NAIVE_QMM_H
+
+void qmm_space_waste(float l_scale, float r_scale, float result_scale, uint4x1_t l_offset, uint4x1_t r_offset, 
+		uint4x1_t result_offset, uint4x1_t* l_int_mat, uint4x1_t* r_int_mat, uint4x1_t* result_int_mat, 
+		int n, int k, int m);
+
+#endif
