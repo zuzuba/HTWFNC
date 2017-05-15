d33fb35b2e8dd8d08167fbbdf0063fea79ef669f
Matteo Turchetta <matteotu@inf.ethz.ch>
2017-05-15 22:57:01 +0200

    MNIST computes min and max of resulting matrix

--- MNIST.py
+++ MNIST.py
@@ -20,7 +20,7 @@ train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 # Train
-for _ in range(10000):
+for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 weights = W.eval()
@@ -35,6 +35,9 @@ np.savetxt('data/weight.csv', weights, delimiter=', ', newline='\n')
 x_test = np.hstack((mnist.test.images[:n_points, :], 0.5 *
                     np.ones((n_points, 2))))
 
+tmp = np.dot(x_test, weights)
+    
+print("result max: {}\nResult min: {}".format(np.max(tmp), np.min(tmp)))
 np.savetxt('data/x_test.csv', x_test, delimiter=', ',
            newline='\n')
 np.savetxt('data/y_test.csv', mnist.test.labels[:1000, :], delimiter=', ',
