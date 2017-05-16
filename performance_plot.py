1e27adbffe5025da011f881c9c3c7db2249a5bef
Luca Corinzia <luca.corinzia@gmail.com>
2017-05-16 17:40:35 +0200

    modified performance_plot to account for both qmm and quantize

--- performance_plot.py
+++ performance_plot.py
@@ -3,9 +3,11 @@ import os
 import matplotlib.pyplot as plt
 
 data_folder = './data'
+plot_folder = './plots'
 
-dim, performance = np.loadtxt(os.path.join(data_folder, "naive_perf.dat"), unpack=True)
 peak = 4
+
+dim, performance = np.loadtxt(os.path.join(data_folder, "perf_quantize.dat"), unpack=True)
 
 #TODO: measure or set by hand peak performance
 
@@ -34,7 +36,29 @@ ax.text(dim[-3],11/10*performance[-1],'quantize_google',color='k')
 
 plt.show()
 
-plot_folder = './plots'
-fig.savefig(os.path.join(plot_folder, 'Performance_q.eps'), format='eps')
-fig.savefig(os.path.join(plot_folder, 'Performance_q.png'), format='png',
-            dpi=200)
+fig.savefig(os.path.join(plot_folder, 'Performance_quantize.eps'), format='eps')
+fig.savefig(os.path.join(plot_folder, 'Performance_quantize.png'), format='png', dpi=200)
+
+
+dim, performance = np.loadtxt(os.path.join(data_folder, "perf_qmm.dat"), unpack=True)
+
+fig=plt.figure()
+ax = plt.subplot(111)
+plt.plot(dim, performance,'bd')
+plt.plot(dim, performance,'b')
+plt.plot(dim, peak*np.ones(dim.shape),'r')
+plt.xlim(dim[0], dim[-1])
+plt.yscale("log")
+plt.xlabel("n")
+label = ax.set_ylabel('[flops/cycle]', rotation = 0)
+ax.yaxis.set_label_coords(0, 1.05)
+plt.grid()
+plt.title("Performance of qmm VS matrix dimension")
+plt.ylim(1e-3,peak+1)
+ax.text(dim[-3],11/10*performance[-1],'naive',color='b')
+ax.text(dim[-5],7/10*peak,'scalar peak performance',color='r')
+
+plt.show()
+
+fig.savefig(os.path.join(plot_folder, 'Performance_qmm.eps'), format='eps')
+fig.savefig(os.path.join(plot_folder, 'Performance_qmm.png'), format='png', dpi=200)
