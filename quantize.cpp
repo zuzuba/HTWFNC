13e78b86595101feaf75b26d9bf3459287b0b5e7
Lldenaurois <Lldenaurois@gmail.com>
2017-05-28 21:59:22 +0200

    Updated comments with WIP version

--- quantize.cpp
+++ quantize.cpp
@@ -64,6 +64,20 @@ void quantize_AVX(float *d, uint4x4_t *q, float *min, float *max, int rows, int 
 	__m256 upper_row;
 	__m256 lower_row;
 
+//BEGIN_WIP
+	/*This part is currently work in progress. Since the struct seems to be reading bits in reverse order, 
+	 * we should be able to remedy the situation by simply reversing the order in which we shift elements.
+	 * Currenlty, if we have two rows of the matrix, we place 8 elements of each row in a register, upper_row and lower_row
+	 * Then, we perform quantization and then staturate and round. Subsequently, we shift the least-significant 4-bits
+	 * by top_shift (for the upper_row) and bottom_shift (for the lower_row)
+	 * Using the top_shift and bottom_shift below should reverse the order of the members in the struct when we write straight
+	 * to the memory location of the struct, but it does not.
+	 * Very Frustrating.
+	 */
+	//__m256 top_shift = _mm256_set_ps(16.0,1.0,16.0,1.0,16.0,1.0,16.0,1.0);
+	//__m256 bottom_shift = _mm256_set_ps(4096.0,256.0,4096.0,256.0,4096.0,256.0,4096.0,256.0);
+//END_WIP
+	
 	__m256 top_shift = _mm256_set_ps(256.0,4096.0,256.0,4096.0,256.0,4096.0,256.0,4096.0);
 	__m256 bottom_shift = _mm256_set_ps(1.0,16.0,1.0,16.0,1.0,16.0,1.0,16.0);
 
@@ -121,32 +135,64 @@ void quantize_AVX(float *d, uint4x4_t *q, float *min, float *max, int rows, int 
 			/* Now we or the two registers upper_row and lower_row
 			 * The result is that i1 and i3 are in the elements of even index, while i2 and i4 are in the elements of 
 			 * uneven index. That is:
-			 * upper_row = [ (ur1<<12) | (lr1<<4) , (ur2<<8) | (lr2) , (ur3<<12) | (lr3<<4) , (ur4<<8) | (lr4) , ... ]
+			 * lower_row = [ (ur1<<12) | (lr1<<4) , (ur2<<8) | (lr2) , (ur3<<12) | (lr3<<4) , (ur4<<8) | (lr4) , ... ]
 			 */
 
-        		upper_row = _mm256_or_si256( upper_row,lower_row);
+			lower_row = _mm256_or_si256(upper_row, lower_row);
 
-			/* Now, of course we would still need to perform the following
-			 * upper_row = _mm256_or_si256( upper_row, _mm256_leftshift_si256( (_m256*) upper_row) );
-			 * That is, we would need to OR the elements of even index in upper_row with the elements of uneven index in upper_row.
-			 * This is not at all trivial, and has to be done with multiple shuffle and blend operations. The total number of operations required is
-			 * greater than the scalar code of just ORing the uint16_t's below, so I just went with that option.
-			 * Therefore, I store the AVX register here in the array: uint16_t fi[16]
+			/* Now we need to OR the lower_row register with itself (shifted left 32-bits).
+			 * BUT, we don't actually need the entire register to be shifted left, we only need every other element to be shifted 32-bits
+			 * to the left of where it is in lower_row (because we only need to OR pairwise neighoring 32-bit elements). Coincidentally, we can achieve this
+			 * by actually cycling the elements in a lane, i.e. 0,1,2,3 -> 1,2,3,0 for both lanes. Therefore, we can just use the permute functionality
+			 * that places the 32-bit ints at index 1,3,5,7 in the positions at index 0,2,4,6
+			 * NOTE:
+			 * lower_row = [ (ur1<<12) | (lr1<<4) , (ur2<<8) | (lr2) , (ur3<<12) | (lr3<<4) , (ur4<<8) | (lr4) , ... ]
+			 * _mm256_permute_ps( lower_row, 57 ) =
+			 * 		[ (ur2<<8) | (lr2) , (ur3<<12) | (lr3<<4) , (ur4<<8) | (lr4) , (ur1<<12) | (lr1<<4) , ... ] (analogous for the other lane)
+			 */
+
+			upper_row = _mm256_or_si256(lower_row, _mm256_permute_ps(lower_row, 57 ) );
+
+			/* Since we only
+			 * 1. Require the 16 most significant bits of every 32-bit int
+			 * 2. Only require every other 32-bit register in the first place
+			 * We store the entire AVX register here in the array: uint16_t fi[16]
+			 * We will only need fi[0], fi[4], fi[8], fi[12]
 			 */
 
 			_mm256_store_si256( (__m256i*)&fi, upper_row);
 
-			/* Now, recall that we were working with 32-bit floats, but with saturation and rounding, we ended up with 32-bit ints (of which only the first
+		//BEGIN_DEBUGGING
+
+			/* For debugging purposes, we can keep lower_row, which is "almost" perfect
+			 * If you use gi instead of fi, you need to pairwise OR together neigboring 32-bit registers (more precisely OR together every other uint16_t
+			 * corresponding to neigboring slots in the AVX register
+			 */
+
+			//_mm256_store_si256( (__m256i*)&gi, lower_row);
+			 
+			/* IF USING gi: Now, recall that we were working with 32-bit floats, but with saturation and rounding, we ended up with 32-bit ints (of which only the first
 			 * 4-bits could every be set. The array fi is an array of uint16_t's, so therefore every other element (the elements with uneven index) in fi is
 			 * always 0. Therefore, fi[0] and fi[2] are in fact the two pieces that we need to compute the first struct.
 			 * We OR fi[0] and fi[2] in temp to arrive that the desired values.
 			 */
 
-			temp = (fi[0] | fi[2]);
+			//temp = (gi[0] | gi[2]);
 			
+		//END_DEBUGGING
+
+			/* IF USING fi: Nothing to be done. Every 4th uint16_t is the exact 16-bits needed to encode the 4 4-bit integers
+			 */
+
+			temp = fi[0];
+			
+		//BEGIN_WIP
+
 			/* TODO: This, doesn't work! Even though it should */
 			//UNCOMMENT HERE IF YOU WOULD LIKE TO IMPROVE THE CODE
 			//*(uint16_t*)(q+(i/2*columns/2 + j/2)) = temp;
+
+		//END_WIP
 
 			/* This does work! Therefore, you see that the values in temp are correct.
 			 * Shifting temp by 12 bits, i.e. temp>>12, gives us the 4 most siginificant bits
@@ -162,19 +208,37 @@ void quantize_AVX(float *d, uint4x4_t *q, float *min, float *max, int rows, int 
 			q[i/2*columns/2 + j/2].i3 = temp>>4;
 			q[i/2*columns/2 + j/2].i4 = temp;
 			
-			// Same construction as above
+		//BEGIN_DEBUGGING
+			/*
+			bool b;
+			uint16_t *t = (uint16_t*)(q+(i/2*columns/2+j/2));
+			std::cout << "BITS: ";
+			for (int k=15; k>=0; k--){
+				b = (*t & (1<<k)) > 0 ? 1 : 0;
+				std::cout << b;	
+			}
+			std::cout << temp << " " << fi[0] << std::endl;
+			std::cout << std::endl << "DECIMALS: " << (temp>>12 & 15) << " " << (temp>>8 & 15) << " " << (temp>>4 & 15) << " " << (temp & 15);
+			std::cout << std::endl << "Member Values: " << (int) q[i/2*columns/2+j/2].i1 << " " << (int) q[i/2*columns/2 + j/2].i2 << " " << (int) q[i/2*columns/2 + j/2].i3 << " " << (int) q[i/2*columns/2 + j/2].i4  << std::endl << std::endl;
+			*/
+		//END_DEBUGGING
 
-			temp = (fi[4] | fi[6]);
+			
+
+			//temp = (gi[4] | gi[6]); //IF USING DEBUGGING gi as oppose to complete fi
+			temp = fi[4];
 			q[i/2*columns/2 + j/2 + 1].i1 = temp>>12;
 			q[i/2*columns/2 + j/2 + 1].i2 = temp>>8;
 			q[i/2*columns/2 + j/2 + 1].i3 = temp>>4;
 			q[i/2*columns/2 + j/2 + 1].i4 = temp;
-			temp = (fi[8] | fi[10]);
+			//temp = (gi[8] | gi[10]); //IF USING DEBUGGING gi as oppose to complete fi
+			temp = fi[8];
 			q[i/2*columns/2 + j/2 + 2].i1 = temp>>12;
 			q[i/2*columns/2 + j/2 + 2].i2 = temp>>8;
 			q[i/2*columns/2 + j/2 + 2].i3 = temp>>4;
 			q[i/2*columns/2 + j/2 + 2].i4 = temp;
-			temp = (fi[12] | fi[14]);
+			//temp = (gi[12] | gi[14]); //IF USING DEBUGGING gi as oppose to complete fi
+			temp = fi[12];
 			q[i/2*columns/2 + j/2 + 3].i1 = temp>>12;
 			q[i/2*columns/2 + j/2 + 3].i2 = temp>>8;
 			q[i/2*columns/2 + j/2 + 3].i3 = temp>>4;
