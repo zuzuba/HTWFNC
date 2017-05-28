3fb07bced39d69295ec115cba6310c7688ffa08f
Luca Corinzia <luca.corinzia@gmail.com>
2017-05-28 10:58:04 +0200

    add qmm_kernel_AVX_unrolled

--- qmm.h
+++ qmm.h
@@ -21,4 +21,8 @@ void qmm_trick_AVX(float l_scale, float r_scale, float result_scale, uint4x4_t l
 	uint4x4_t result_offset, uint4x4_t* l_int_mat, uint4x4_t* r_int_mat, uint4x4_t* result_int_mat, 
 	int n, int k, int m);
 
+void qmm_trick_AVX_unrolled(float l_scale, float r_scale, float result_scale, uint4x4_t l_offset, uint4x4_t r_offset, 
+	uint4x4_t result_offset, uint4x4_t* l_int_mat, uint4x4_t* r_int_mat, uint4x4_t* result_int_mat, 
+	int n, int k, int m);
+
 #endif
