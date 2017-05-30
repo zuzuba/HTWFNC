d47c77365d822ea66cc77bc2d23f8cc676a61808
Luca Corinzia <luca.corinzia@gmail.com>
2017-05-30 17:15:16 +0200

    add qmm function trick_blocking + avx

--- qmm.h
+++ qmm.h
@@ -17,11 +17,7 @@ void qmm_trick_blocking(float l_scale, float r_scale, float result_scale, uint4x
 	uint4x4_t result_offset, uint4x4_t* l_int_mat, uint4x4_t* r_int_mat, uint4x4_t* result_int_mat, 
 	int n, int k, int m);
 
-void qmm_trick_AVX(float l_scale, float r_scale, float result_scale, uint4x4_t l_offset, uint4x4_t r_offset, 
-	uint4x4_t result_offset, uint4x4_t* l_int_mat, uint4x4_t* r_int_mat, uint4x4_t* result_int_mat, 
-	int n, int k, int m);
-
-void qmm_trick_AVX_unrolled(float l_scale, float r_scale, float result_scale, uint4x4_t l_offset, uint4x4_t r_offset, 
+void qmm_trick_blocking_AVX(float l_scale, float r_scale, float result_scale, uint4x4_t l_offset, uint4x4_t r_offset, 
 	uint4x4_t result_offset, uint4x4_t* l_int_mat, uint4x4_t* r_int_mat, uint4x4_t* result_int_mat, 
 	int n, int k, int m);
 
